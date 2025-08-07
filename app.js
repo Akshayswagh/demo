5b0d800e3d124af4c59c943dd2c47cf06288493b
Akshayswagh <waghakshay186@gmail.com>
2025-08-07 18:15:49 +0530

    commented

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
 });
-app.use("/listings", listingRouter);
+// app.use("/listings", listingRouter);
 // app.use("/listings/:id/reviews", reviewRouter);
 app.use("/user", userRouter);
 
