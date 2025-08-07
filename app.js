601a7aecc6fd934069be0b48d610a9b7329f8f05
Akshayswagh <waghakshay186@gmail.com>
2025-08-07 19:43:55 +0530

    this is new code 4

--- app.js
+++ app.js
@@ -80,12 +80,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// app.use((req, res, next) => {
-//   res.locals.success = req.flash("success");
-//   res.locals.error = req.flash("error");
-//   res.locals.currentUser = req.user;
-//   next();
-// });
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get("/", (req, res) => res.send("hello"));
 
