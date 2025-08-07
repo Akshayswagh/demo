a30d70b0a2b0d5b5590f57476d3cceb2e444cfe5
Akshayswagh <waghakshay186@gmail.com>
2025-08-07 19:46:45 +0530

    this is new code 5

--- app.js
+++ app.js
@@ -100,10 +100,10 @@ app.use("/user", userRouter);
 // });
 
 // Error Handler
-// app.use((err, req, res, next) => {
-//   let { statusCode = 500, message = "Something went wrong" } = err;
-//   res.status(statusCode).render("error.ejs", { message });
-// });
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
 
 app.listen(3000, () => {
   console.log("server is listening to port 3000");
