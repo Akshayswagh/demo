f526c83a9d1befbba8812037ab0deb6ab574d7b3
Akshayswagh <waghakshay186@gmail.com>
2025-08-07 18:52:05 +0530

    shorten

--- app.js
+++ app.js
@@ -1,111 +1,13 @@
-// =============================
-//      START of app.js
-// =============================
-if (process.env.NODE_ENV != "production") {
-  require('dotenv').config();
-}
-  
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
-const session = require("express-session");
-const MongoStore = require('connect-mongo');
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
 
-// REQUIRE YOUR ROUTERS
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
+// Use the port Render provides, or 3000 for local testing
+const PORT = process.env.PORT || 3000;
 
-const dbUrl = process.env.ATLASDB_URL;
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-   console.log(err);
-  });
-
-async function main() {
-  await mongoose.connect(dbUrl);
-}
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
-
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
-  touchAfter: 24 * 3600,
+app.get('/', (req, res) => {
+  res.send('Hello from the minimal server! It is working!');
 });
 
-store.on("error", (err) => { // Added err parameter
-  console.log("session store error", err);
+app.listen(PORT, () => {
+  console.log(`Minimal server is listening on port ${PORT}`);
 });
-
-const sessionOptions = {
-  store,
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { // Fixed typo: 'Cookie' to 'cookie'
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // Fixed typo: 'expores' to 'expires' and used new Date()
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-  },
-};
-
-app.use(session(sessionOptions));
-app.use(flash());
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-
-app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  res.locals.currentUser = req.user;
-  next();
-});
-
-// KEEP THESE COMMENTED FOR NOW
-// app.use("/listings", listingRouter);
-// app.use("/listings/:id/reviews", reviewRouter);
-// app.use("/user", userRouter);
-
-// 404 Handler
-app.all("*", (req, res, next) => {
-  next(new ExpressError(404,"Page Not Found!"));
-});
-
-// Error Handler
-app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs", { message });
-});
-
-app.listen(3000, () => {
-  console.log("server is listening to port 3000");
-}); 
-// =============================
-//      END of app.js
-// =============================
