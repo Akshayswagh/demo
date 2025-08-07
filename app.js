01d41dcac9b248a61b06901e047dd92d68e9411b
Akshayswagh <waghakshay186@gmail.com>
2025-08-07 18:39:58 +0530

    solved on local

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// =============================
+//      START of app.js
+// =============================
 if (process.env.NODE_ENV != "production") {
   require('dotenv').config();
 }
@@ -16,6 +19,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+// REQUIRE YOUR ROUTERS
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
@@ -48,8 +52,8 @@ const store = MongoStore.create({
   touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
-  console.log("session store error",err);
+store.on("error", (err) => { // Added err parameter
+  console.log("session store error", err);
 });
 
 const sessionOptions = {
@@ -57,16 +61,12 @@ const sessionOptions = {
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
-  Cookie: {
-    expores: Date.now() + 7 * 24 * 60 * 60 * 1000,
+  cookie: { // Fixed typo: 'Cookie' to 'cookie'
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // Fixed typo: 'expores' to 'expires' and used new Date()
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    },
+  },
 };
-
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -86,24 +86,26 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
 });
+
+// KEEP THESE COMMENTED FOR NOW
 // app.use("/listings", listingRouter);
 // app.use("/listings/:id/reviews", reviewRouter);
-// // app.use("/user", userRouter);
+// app.use("/user", userRouter);
 
-
-
-
-// 404
+// 404 Handler
 app.all("*", (req, res, next) => {
   next(new ExpressError(404,"Page Not Found!"));
 });
 
+// Error Handler
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { message });
-  // res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
   console.log("server is listening to port 3000");
 }); 
+// =============================
+//      END of app.js
+// =============================
